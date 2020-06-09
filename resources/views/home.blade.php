6ad8cf96b3e5ca28364a2ae034986dadd965094a
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2020-06-09 23:26:07 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,14 +34,14 @@
                             </div>
                             <h1 data-aos="fade-up"><br><b>ปฏิวัติพื้นที่ทำงานของคุณ</b></h1>
                             <h5 data-aos="fade-up" data-aos-delay="400">ไม่ว่าคุณจะเป็นองค์กรที่มีรากฐานมั่นคงแล้วหรือสตาร์ทอัพที่กำลังโต ลองมาสำรวจพื้นที่ที่จะสร้างแรงบันดาลใจให้คุณสร้างสรรค์ผลงานที่สร้างผลกระทบที่สุด</h5>
-                            <div data-aos="fade-up" data-aos-delay="800">
+                            <div data-aos="fade-up" data-aos-delay="800"><br>
                               <a href="" class="btn-get-started scrollto">เริ่มต้น</a>
                             </div>
                             <div>
                               <h4><br></h4>
                             </div>
                           </div>
-                    </section><!-- End Hero -->
+                    </section>
             </div>
 
             <hr>
@@ -64,9 +64,212 @@
                           <a aria-label="อ่านเพิ่มเติมเกี่ยวกับมาตรการตอบสนองของ WeWork" class="ray-link ray-link--with-arrow" href="https://www.wework.com/info/navigating-the-future-of-the-workplace">อ่านเพิ่มเติมเกี่ยวกับมาตรการตอบสนองของ WeWork</a>
                         </div><br>
                     </div>
-              </section><!-- End Hero -->
+              </section>
+            </div><br>
+
+            <div class="card" style="border: 0px">
+
+              <section style="background: #f7f7f7;">
+
+                <div class="card" style="width: 18rem;position:absolute; margin-left:400px;border: 1px solid #DCDCDC;border-radius: 5px;">
+                    <img src="https://locations-api-production.imgix.net/locations/image/834625e4-1b49-11e9-845b-1202be33576a/0-3.jpg?auto=format%20compress&amp;fit=crop&amp;q=50&amp;w=1000&amp;h=563" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example </p>
+                    </div>
+                  </div> 
+
+                <div class="col-md-4" ><br>
+                  <div>อาคารใกล้คุณ </div><br>
+                    <h1><b>5 แห่งใน Bangkok</b></h1><br>
+                    <div>
+                      <a href="">ดูทั้งหมดใน Bangkok</a>
+                    </div><br><br><br><br>
+                </div>
+                
+              </section>
+
+            </div><br>
+
+            <div class="card" style="border: 0px">
+
+              <section >
+
+                <div class="card" style="width: 18rem;position:absolute; margin-left:392px;border: 1px solid #DCDCDC;border-radius: 5px;">
+                    <img src="https://ctfassets.imgix.net/vh7r69kgcki3/2I0PdTXSFMaGjVzDOkwA9p/acfb401bf1b74cfbabed6cf2d222f19b/Web_150DPI-20190723_WeWork_Seolleung_III_12.jpg?auto=format%20compress&amp;fit=crop&amp;q=50&amp;w=1000&amp;h=563" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h4 class="card-title"><a href=""> ห้องชุดสำนักงาน</a></h4>
+                      <p class="card-text">สำนักงานสำหรับทีมขนาดใหญ่ หรือทีมขนาดกลางที่มีความหนาแน่นน้อยลงมา ซึ่งมาพร้อมกับห้องประชุม ห้องทำงานผู้บริหาร และการสนับสนุนการปฏิบัติงาน</p>
+                    </div>
+                  </div>
+
+                  <div class="card" style="width: 18rem;position:absolute; margin-left:707px;border: 1px solid #DCDCDC;border-radius: 5px;">
+                    <img src="https://ctfassets.imgix.net/vh7r69kgcki3/2I0PdTXSFMaGjVzDOkwA9p/acfb401bf1b74cfbabed6cf2d222f19b/Web_150DPI-20190723_WeWork_Seolleung_III_12.jpg?auto=format%20compress&amp;fit=crop&amp;q=50&amp;w=1000&amp;h=563" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h4 class="card-title"><a href=""> ห้องชุดสำนักงาน</a></h4>
+                      <p class="card-text">สำนักงานสำหรับทีมขนาดใหญ่ หรือทีมขนาดกลางที่มีความหนาแน่นน้อยลงมา ซึ่งมาพร้อมกับห้องประชุม ห้องทำงานผู้บริหาร และการสนับสนุนการปฏิบัติงาน</p>
+                    </div>
+                  </div>
+
+                  <div class="card" style="width: 18rem;position:absolute; margin-left:1020px;border: 1px solid #DCDCDC;border-radius: 5px;">
+                    <img src="https://ctfassets.imgix.net/vh7r69kgcki3/2I0PdTXSFMaGjVzDOkwA9p/acfb401bf1b74cfbabed6cf2d222f19b/Web_150DPI-20190723_WeWork_Seolleung_III_12.jpg?auto=format%20compress&amp;fit=crop&amp;q=50&amp;w=1000&amp;h=563" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h4 class="card-title"><a href=""> ห้องชุดสำนักงาน</a></h4>
+                      <p class="card-text">สำนักงานสำหรับทีมขนาดใหญ่ หรือทีมขนาดกลางที่มีความหนาแน่นน้อยลงมา ซึ่งมาพร้อมกับห้องประชุม ห้องทำงานผู้บริหาร และการสนับสนุนการปฏิบัติงาน</p>
+                    </div>
+                  </div>
+
+                <div class="col-md-3" ><br>
+                    <h2><b>การออกแบบที่ตอบสนองความต้องการอย่างครบครัน</b></h2><br>
+                    <div>
+                      <p>สถานที่ทำงานร่วมกันคือผลงานที่เราบรรจงสร้างสรรค์ เราพร้อมให้บริการพื้นที่ที่เหมาะกับคุณ ตั้งแต่สำนักงานส่วนตัวไปจนถึงสำนักงานใหญ่ที่พรั่งพร้อม</p>
+                    </div>
+                    <div>
+                      <a href="">ดูโซลูชันทั้งหมด</a>
+                    </div><br>
+                </div>
+                
+              </section>
 
             </div>
+
+            <div class="card" style="border: 0px">
+              <div>
+                <h2><br>
+                    <b> &nbsp; เรื่องราวความสำเร็จของลูกค้า</b>
+                </h2>
+                <p></p>
+              </div>
+            </div>         
+
+            <div class="card" style="border: 0px">
+
+              <section style="background: #f7f7f7;">
+
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div>
+                        <div>
+                          <img width="75" height="75" src="https://ctfassets.imgix.net/vh7r69kgcki3/1dbIJcmyiy5sLcadW6KtE5/3bae7e4f306beb351a88ac460c110971/GE_Logo.png?auto=format%20compress&fit=crop&q=50&w=200">
+                        </div><br>
+                          <h2><b>"เราประสบความสำเร็จในการทำโครงการประหยัดค่าใช้จ่าย ในขณะเดียวกันก็ปรับวัฒนธรรมไปสู่การทำงานร่วมกันให้ดีขึ้นได้สำเร็จด้วย"</b></h2><br>
+                          <div>
+                            FRANCIS VAN PARYS ประธานและซีอีโอ GE HEALTHCARE KOREA
+                          </div>
+                          <div>
+                            <a href="">เรียนรู้เพิ่มเติม</a>
+                          </div><br>
+                      </div>
+                    </div>
+                    
+                    <div class="col-md-6">
+                      <div>
+                        <div>
+                            <img width="110" height="30" src="https://ctfassets.imgix.net/vh7r69kgcki3/7yAQhnqtkFdi34Luj6ZIzJ/5fefe0bd181b32f23504bfdbe50021db/royal-bank-canade_logo_3.png?auto=format%20compress&fit=crop&q=50&w=200">
+                        </div><br>
+                          <p>"WeWork ช่วยกำจัดอุปสรรคใหญ่ออกไปได้ในเรื่องของการหาพื้นที่สำนักงานและการเจรจากับเจ้าของที่"<br><br>
+                          
+                            NIRANJAN VIVEKANANDAN รองประธานฝ่ายกลยุทธ์ที่ธนาคารพาณิชย์ RBC</p>
+                          
+                          <div>
+                            <a href="">เรียนรู้เพิ่มเติม</a>
+                          </div>
+                        </div>
+                        <hr>
+                        <div>
+                        <div>
+                            <img width="110" height="30" src="https://ctfassets.imgix.net/vh7r69kgcki3/4jW89SumohqjzK9woLhBpf/6e8c67d271294aef709a34f95fa7e1c0/Standard_Chartered_Logo.png?auto=format%20compress&fit=crop&q=50&w=200">
+                        </div><br>
+                          <p>"ห้องปฏิบัติการคิดค้นนวัตกรรมของเรารวบรวมไอเดีย ทักษะ และที่สำคัญคือคนเก่ง ๆ มาไว้ด้วยกันเพื่อสร้างสรรค์สิ่งใหม่ ๆ"<br><br>
+                          
+                            ALEX MANSON ผู้อำนวยการระดับโลกของ SC VENTURES, STANDARD CHARTERED BANK</p>
+                          
+                          <div>
+                            <a href="">เรียนรู้เพิ่มเติม</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              </section>
+
+            </div>
+
+            
+              <div class="card" style="border: 0px">
+              <div>
+                <br><h2>
+                    <b> &nbsp; ข่าวสาร ข้อมูลใหม่ และข้อมูลเชิงลึก</b>
+                </h2>
+              </div>
+            </div>    
+
+            <div class="card" style="border: 0px">
+
+              <section>
+
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-6" style="margin-left:-110px;">
+                        <img width="630" height="400" src="https://ctfassets.imgix.net/vh7r69kgcki3/5QhlwTyEXHyGMuZYVdAaOc/f08b765cd6e645b3b3589774c8c3604d/20190919-IdeasbyWe-ManagedbyQ-CompCulture-01.png?auto=format%20compress&fit=crop&q=50&w=900&h=506">
+                    </div>
+                    
+                    <div class="col-md-6" style="margin-left:110px;">
+                      <div>
+                        <h1><a href=""><br>ส่งเสริมให้พนักงานที่ทำงานทางไกลมีส่วนร่วมด้วยวัฒนธรรมของบริษัท</h1></a><br>
+                          ลองมาดูวิธีกระตุ้นให้พนักงานทางไกลมีส่วนร่วมด้วยค่านิยมและวัฒนธรรมของบริษัท
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+
+            <hr>
+
+            <div class="card" >
+
+              <section>
+
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-4" >
+                        <div class="card" style="border: 1px solid #DCDCDC;border-radius: 5px;">
+                          <img src="https://ctfassets.imgix.net/vh7r69kgcki3/2I0PdTXSFMaGjVzDOkwA9p/acfb401bf1b74cfbabed6cf2d222f19b/Web_150DPI-20190723_WeWork_Seolleung_III_12.jpg?auto=format%20compress&amp;fit=crop&amp;q=50&amp;w=1000&amp;h=563" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h4 class="card-title"><a href=""> ห้องชุดสำนักงาน</a></h4>
+                            <p class="card-text">สำนักงานสำหรับทีมขนาดใหญ่ หรือทีมขนาดกลางที่มีความหนาแน่นน้อยลงมา ซึ่งมาพร้อมกับห้องประชุม ห้องทำงานผู้บริหาร และการสนับสนุนการปฏิบัติงาน</p>
+                          </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4" >
+                        <div class="card" style="border: 1px solid #DCDCDC;border-radius: 5px;">
+                          <img src="https://ctfassets.imgix.net/vh7r69kgcki3/2I0PdTXSFMaGjVzDOkwA9p/acfb401bf1b74cfbabed6cf2d222f19b/Web_150DPI-20190723_WeWork_Seolleung_III_12.jpg?auto=format%20compress&amp;fit=crop&amp;q=50&amp;w=1000&amp;h=563" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h4 class="card-title"><a href=""> ห้องชุดสำนักงาน</a></h4>
+                            <p class="card-text">สำนักงานสำหรับทีมขนาดใหญ่ หรือทีมขนาดกลางที่มีความหนาแน่นน้อยลงมา ซึ่งมาพร้อมกับห้องประชุม ห้องทำงานผู้บริหาร และการสนับสนุนการปฏิบัติงาน</p>
+                          </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4" >
+                        <div class="card" style="border: 1px solid #DCDCDC;border-radius: 5px;">
+                          <img src="https://ctfassets.imgix.net/vh7r69kgcki3/2I0PdTXSFMaGjVzDOkwA9p/acfb401bf1b74cfbabed6cf2d222f19b/Web_150DPI-20190723_WeWork_Seolleung_III_12.jpg?auto=format%20compress&amp;fit=crop&amp;q=50&amp;w=1000&amp;h=563" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h4 class="card-title"><a href=""> ห้องชุดสำนักงาน</a></h4>
+                            <p class="card-text">สำนักงานสำหรับทีมขนาดใหญ่ หรือทีมขนาดกลางที่มีความหนาแน่นน้อยลงมา ซึ่งมาพร้อมกับห้องประชุม ห้องทำงานผู้บริหาร และการสนับสนุนการปฏิบัติงาน</p>
+                          </div>
+                        </div>
+                    </div>
+
+                  </div>
+                </div>
+              </section>
+
+            </div>
+
         </div>
     </div>
 
