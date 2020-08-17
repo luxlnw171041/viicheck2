a9c4ebc266b7805b96f4b47fd7699a2f7eee5000
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2020-08-17 10:53:22 +0700

    Update DealersController.php

--- app/Http/Controllers/API/DealersController.php
+++ app/Http/Controllers/API/DealersController.php
@@ -55,12 +55,12 @@ class DealersController extends Controller
 		            $lng = $event['message']['longitude'];
 		            $your_location = $lat . " / " . $lng ;
 
-		            if(!empty($lat) or !empty($lng)){
+		   //          if(!empty($lat) or !empty($lng)){
 
-						$dealer = DB::select("SELECT name_dealers,location,province,( 3959 * acos( cos( radians($lat) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians($lng) ) + sin( radians($lat) ) * sin( radians( latitude ) ) ) ) AS distance FROM dealers  HAVING distance < 2000 ORDER BY distance LIMIT 0 , 5", []);
+					// 	$dealer = DB::select("SELECT name_dealers,location,province,( 3959 * acos( cos( radians($lat) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians($lng) ) + sin( radians($lat) ) * sin( radians( latitude ) ) ) ) AS distance FROM dealers  HAVING distance < 2000 ORDER BY distance LIMIT 0 , 5", []);
 
-			        	return response()->json($dealer);
-					}
+			  //       	return response()->json($dealer);
+					// }
 
 		            // Get replyToken
 		            $replyToken = $event['replyToken'];
